8b4932ffef2ed859d6d1905cb0e64f9f036c1155
Malcolm Ryan <malcoln.ryan@mq.edu.au>
2022-08-01 12:42:07 +1000

    Playing with rotation

--- COMP2160 Demo Week 02/Assets/Scripts/Move.cs
+++ COMP2160 Demo Week 02/Assets/Scripts/Move.cs
@@ -4,12 +4,22 @@ using UnityEngine;
 
 public class Move : MonoBehaviour
 {
-    [SerializeField] private float speed = 3.1f;
+    [SerializeField] private float speed = 0f;  // m/s
+    [SerializeField] private float rotationSpeed = 60; // deg/s
+    [SerializeField] private Vector3 rotationAxis = Vector3.up; // world frame
+
 
     void Update()
     {
         // move the ship forwards
         Vector3 move = speed * Vector3.forward * Time.deltaTime;
-        transform.Translate(move, Space.Self);        
+        // transform.Translate(move, Space.Self);        
+        transform.localPosition = transform.localPosition + move;
+
+        float angle = rotationSpeed * Time.deltaTime;
+        Quaternion q = Quaternion.AngleAxis(angle, rotationAxis);
+
+        // transform.Rotate(rotationAxis, angle);
+        transform.localRotation = q * transform.localRotation;
     }
 }
